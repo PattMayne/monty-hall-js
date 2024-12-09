441ef730a63e8d9ee92629d2181d5c65253bb7a6
pattmayne <pattmayne@gmail.com>
2024-12-09 07:52:40 -0500

    more css referencing
    fonts returned
    
    re-added images
    
    trimmed images
    
    fonts updated

---
+++ README.md
@@ -0,0 +1,17 @@
+# MONTY HALL PROBLEM
+
+This is an implementation of the classic probability puzzle, the Monty Hall problem. It's a purely client-side game written in JavaScript, HTML, and CSS. The user can play the game repeatedly, and the scores are tallied below the game buttons (as long as they don't refresh the browser).
+
+The puzzle is figuring out whether the user should switch doors or keep doors, or if neither option offers statistical advantages.
+
+There's nothing to install. You can visit the live site (https://montyhall.pattmayne.com/) or download the package and open index.html in any browser.
+
+The fonts used in the game include **Alte Haas Grotesk** fonts (https://www.dafont.com/alte-haas-grotesk.font, https://www.yannlecoroller.com/) and **Bolwby One** (https://www.fontsquirrel.com/fonts/bowlby-one).
+
+The images (doors, donkey, money) are from https://deepai.org.
+
+## SCREENSHOTS
+
+![pick a door](img/screen_1.png)
+
+![switch or keep](img/screen_2.png)
--- js/app.js
+++ js/app.js
@@ -1,55 +1,38 @@
+/**
+ * Monty Hall Problem.
+ * https://github.com/PattMayne/monty-hall-js
+ * All code written by Matt Payne.
+ */
+
 $(document).foundation()
 
-
-let doors = [{
-        winner: false,
-        open: false,
-        chosen: false,
-        element: null
-    },
-    {
-        winner: false,
-        open: false,
-        chosen: false,
-        element: null
-    },
-    {
-        winner: false,
-        open: false,
-        chosen: false,
-        element: null
-    }]
-
-let gameState = {
-    phase: 0,
-    winningDoor: -1
-}
+let doors, gameState
 
 let closedDoorImages = []
 let openLoserImages = []
 let openWinnerImages = []
 
-let doorImages
-let switchButtons
+let switchButtons, switchButtonYes, switchButtonNo, playAgainButton
+let doorSigns, doorSign_1, doorSign_2, doorSign_3
 
 let phaseMessage
 
-const start = () => {
-    console.log("Hello")
+var switchWins = 0
+var switchLosses = 0
+var keepWins = 0
+var keepLosses = 0
 
-    phaseMessage = document.getElementById("phaseMessage")
 
-    doors[0].element = document.getElementById("door_1")
-    doors[1].element = document.getElementById("door_2")
-    doors[2].element = document.getElementById("door_3")
+function start() {
+    // Get DOM elements
+    doorSigns = document.getElementsByClassName("door_sign")
+    doorSign_1 = document.getElementById("door_sign_1")
+    doorSign_2 = document.getElementById("door_sign_2")
+    doorSign_3 = document.getElementById("door_sign_3")
 
     closedDoorImages[0] = document.getElementById("1_closed")
     closedDoorImages[1] = document.getElementById("2_closed")
     closedDoorImages[2] = document.getElementById("3_closed")
-
-    closedDoorImages[0].onclick = function() { clickDoor(0)}
-    closedDoorImages[1].onclick = function() { clickDoor(1)}
-    closedDoorImages[2].onclick = function() { clickDoor(2)}
 
     openLoserImages[0] = document.getElementById("1_loser")
     openLoserImages[1] = document.getElementById("2_loser")
@@ -59,85 +42,220 @@ const start = () => {
     openWinnerImages[1] = document.getElementById("2_winner")
     openWinnerImages[2] = document.getElementById("3_winner")
 
-    openLoserImages.map(imageElement => {imageElement.style.display = "none"})
-    openWinnerImages.map(imageElement => {imageElement.style.display = "none"})
-
+    playAgainButton = document.getElementById("play_again_button")
+    switchButtonYes = document.getElementById("switch_yes_button")
+    switchButtonNo = document.getElementById("switch_no_button")
     switchButtons = document.getElementsByClassName("switch_button")
-    for(var i=0; i<switchButtons.length; i++) {
-        hideSwitchButtons()
-    }
+    phaseMessage = document.getElementById("phaseMessage")
 
-    doorImages = document.getElementsByClassName("door_img")
-    for(var i=0; i<doorImages.length; i++) {
-        doorImages[i].style.border = "5px solid #000004"
-    }
+    // set onclick functions
+    closedDoorImages[0].onclick = function() { clickDoor(0)}
+    closedDoorImages[1].onclick = function() { clickDoor(1)}
+    closedDoorImages[2].onclick = function() { clickDoor(2)}
 
+    doorSign_1.onclick = function() { clickDoor(0)}
+    doorSign_2.onclick = function() { clickDoor(1)}
+    doorSign_3.onclick = function() { clickDoor(2)}
 
+    switchButtonYes.onclick = function() { switchDoors() }
+    switchButtonNo.onclick = function() { keepDoors() }
+    playAgainButton.onclick = function() { setupGame() }
 
-    // hide open door images
-
-    // pick a winning door
-    // set up the three door objects
-
-    let winningNumber = Math.floor(Math.random() * doors.length)
-    gameState.winningDoor = winningNumber
-    doors[winningNumber].winner = true
-
-    gameState.phase = 1;
-
-    console.log(gameState.winningDoor)
-
-    // prompt to pick a door
-    // doors must be clickable as buttons
-
-
+    setupGame()
 }
 
+/**
+ * Phase 1 functions (Player must choose a door)
+ */
+
+
+/**
+ * Phase 1 action. User clicks a door to select that door.
+ * Then we open one non-chosen, losing door.
+ * @param {int} doorNumber 
+ */
 function clickDoor(doorNumber) {
     if(doorNumber < 3 && doorNumber >= 0 && gameState.phase == 1){
         // select the door
-        console.log("YOU CHOSE: " + doorNumber)
-        doors[doorNumber].chosen = true
-        closedDoorImages[doorNumber].style.border = "5px solid #fff"
+        chooseDoor(doorNumber)
 
         // remove hover pointer capability
         closedDoorImages.map(imgElement => imgElement.classList.remove("hover_door"))
 
+        for(let i=0; i<doorSigns.length; i++){
+            doorSigns[i].classList.remove("hover_door");
+            doorSigns[i].classList.add("normal_pointer");
+        }
+
         let allDoorNumbers = [0, 1, 2]
         let nonChosenDoorsNumbers = []
 
+        // put all NON-chosen door indexes into a list (so we can choose a door to open)
         allDoorNumbers.map(number => number != doorNumber && nonChosenDoorsNumbers.push(number))
-        console.log(nonChosenDoorsNumbers.length + " non-chosens")
 
+        // Now pick a random NON-chosen door (index) to open
         let randomNonChosenDoorNumberIndex = Math.floor(Math.random() * nonChosenDoorsNumbers.length)
         let doorToOpenIndex = -1
 
+        // If that door is actually a winner, get the OTHER door index instead
         if(!doors[nonChosenDoorsNumbers[randomNonChosenDoorNumberIndex]].winner){
             doorToOpenIndex = nonChosenDoorsNumbers[randomNonChosenDoorNumberIndex]
         } else {
             for(let i=0; i<nonChosenDoorsNumbers.length; i++) {
                 let thisDoor = doors[nonChosenDoorsNumbers[i]]
                 if(!thisDoor.winner && !thisDoor.chosen){
-                    console.log("more complicated option")
                     doorToOpenIndex = nonChosenDoorsNumbers[i]
                 }
             }
         }
 
+        // Open whichever losing door you chose (set door to open, and show the appropriate door image)
         doors[doorToOpenIndex].open = true
         showOpenDoors()
 
-        // OPEN one LOSER door
-
-        for(var i=0; i<switchButtons.length; i++) {
-            switchButtons[i].style.visibility = "visible"
-        }
-
-        phaseMessage.innerHTML = "YOU CHOSE DOOR #" + doorNumber + ". <span class='yell'>DO YOU WANT TO SWITCH DOORS?</span>"
+        // show the buttons and text for phase 2
+        let doorNumberToDisplay = doorNumber + 1
+        showSwitchButtons()
+        setSwitchDoor()
+        phaseMessage.innerHTML = "<span class='green'>YOU CHOSE DOOR #" + doorNumberToDisplay + ".</span> <span class='orange'>DO YOU WANT TO SWITCH WITH DOOR #" + String(gameState.switchDoor + 1 )+ "?</span>"
         gameState.phase = 2;
     }
 }
 
+// Find out the index of the door that the user can switch to
+const setSwitchDoor = () => {
+    if(gameState.phase == 1){
+        for (let i=0; i<doors.length; i++) {
+            let door = doors[i]
+    
+            if(!door.open && !door.chosen){
+                gameState.switchDoor = i
+            }
+        }
+    }    
+}
+
+/**
+ * Phase 2 functions (Player chooses whether to switch doors or keep selection)
+ */
+
+
+// Player is switching their selection.
+function switchDoors() {
+    if(gameState.phase == 2){
+        // reset all signs and unchoose all doors
+        for (let i=0; i<doors.length; i++) {
+            doorSigns[i].style.backgroundColor = "#000004"
+            unchooseDoor(i)
+        }
+
+        // choose the NEW door
+        chooseDoor(gameState.switchDoor)
+        // progress the game
+        gameState.switched = true
+        gameState.phase = 3
+        gameState.switchDoor = -1
+        endGame()
+    }
+}
+
+// Player is keeping their selection. Proceed to endgame.
+function keepDoors() {
+    if(gameState.phase == 2){
+        gameState.phase = 3
+        endGame()
+    }
+}
+
+
+/**
+ * Phase 3 functions (game over, show results)
+ */
+
+
+/**
+ * Compare user selection to winning door, display the results, tally the score.
+ */
+function endGame() {
+    if(gameState.phase == 3){
+        hideSwitchButtons()
+        doors.map(door => {
+            door.open = true
+        })
+    }
+    let chosenDoorNumber = -1
+    let winningDoorNumber = -1
+
+    // get the chosen and winning door numbers, for DISPLAY
+    for(let i=0; i<doors.length; i++) {
+        let door = doors[i]
+        if(door.chosen){ chosenDoorNumber = i + 1 }
+        if(door.winner) { winningDoorNumber = i +1 }
+    }
+
+    let playerWon = winningDoorNumber == chosenDoorNumber
+
+    // Tally score
+    if(playerWon){
+        doorSigns[chosenDoorNumber - 1].style.backgroundColor = "#05a000"
+        doorSigns[chosenDoorNumber - 1].style.border = "4px solid #05a000"
+        if(gameState.switched){
+            switchWins++
+        } else {
+            keepWins++
+        }
+    } else {
+        doorSigns[chosenDoorNumber - 1].style.backgroundColor = "#c40000"
+        doorSigns[chosenDoorNumber - 1].style.border = "4px solid #c40000"
+        if(gameState.switched){
+            switchLosses++
+        } else {
+            keepLosses++
+        }
+    }
+
+    let endGameString = "<span class='orange'>"
+    endGameString += gameState.switched ? "YOU SWITCHED TO DOOR #" : "YOU KEPT DOOR #"
+    endGameString += chosenDoorNumber.toString() + ".</span> "
+    endGameString += playerWon ? "<span class='green'>YOU WON!!" : "<span class='red'>YOU LOST!!"
+    endGameString += "</span>"
+    phaseMessage.innerHTML = endGameString
+    showOpenDoors()
+    showScores()
+    hideSwitchButtons()
+    playAgainButton.style.display = ""
+}
+
+
+/**
+ * All phases functions
+ */
+
+// I choose you, door
+const chooseDoor = doorNumber => {
+    doors[doorNumber].chosen = true
+    doorSigns[doorNumber].style.backgroundColor = "#ffa700";
+    doorSigns[doorNumber].style.border = "4px solid #ffa700";
+    doorSigns[doorNumber].style.color = "#000004";
+}
+
+// make "chosen" false, and reset the door sign
+const unchooseDoor = doorNumber => {
+    doors[doorNumber].chosen = false
+    doorSigns[doorNumber].style.backgroundColor = "transparent";
+    doorSigns[doorNumber].style.border = "4px solid #05a000";
+    doorSigns[doorNumber].style.color = "#fff";
+}
+
+// Load the stats into the box
+const showScores = () => {
+    document.getElementById("switch_wins").innerText = switchWins
+    document.getElementById("switch_losses").innerText = switchLosses
+    document.getElementById("keep_wins").innerText = keepWins
+    document.getElementById("keep_losses").innerText = keepLosses
+}
+
+// If any doors have been opened, display the correct image
 const showOpenDoors = () => {
     for(let i=0; i<doors.length; i++){
         let thisDoor = doors[i]
@@ -154,17 +272,64 @@ const showOpenDoors = () => {
     }
 }
 
-const hideSwitchButtons = () => {
+// Make the switch buttons visible
+const showSwitchButtons = () => {
     for(var i=0; i<switchButtons.length; i++) {
-        switchButtons[i].style.visibility = "hidden"
+        switchButtons[i].style.display = ""
     }
 }
 
-document.onload = start()
+// Make the switch buttons invisible
+const hideSwitchButtons = () => {
+    for(var i=0; i<switchButtons.length; i++) {
+        switchButtons[i].style.display = "none"
+    }
+}
 
-/**
- * TO DO:
- * 
- * Put NUMBERS over the doors.
- * Make the NUMBER light up too when a door is selected
- */
+// When you start a new game (does not clear stats)
+function setupGame() {
+    // Reset the UI
+    hideSwitchButtons()
+    closedDoorImages.map(imgElement => imgElement.classList.add("hover_door"))
+    openLoserImages.map(imageElement => {imageElement.style.display = "none"})
+    openWinnerImages.map(imageElement => {imageElement.style.display = "none"})
+    closedDoorImages.map(imageElement => {imageElement.style.display = ""})
+    phaseMessage.innerHTML = "<span class='orange'>PICK A DOOR</span>"
+    playAgainButton.style.display = "none"
+
+    for(let i=0; i<doorSigns.length; i++){
+        doorSigns[i].classList.add("hover_door")
+        doorSigns[i].classList.remove("normal_pointer")
+        if(!!doors){ unchooseDoor(i) }
+    }
+
+    // Reset the game data
+    gameState = {
+        phase: 0,
+        winningDoor: -1,
+        switched: false,
+        switchDoor: -1
+    }
+
+    doors = [{
+        winner: false,
+        open: false,
+        chosen: false
+    },
+    {
+        winner: false,
+        open: false,
+        chosen: false
+    },
+    {
+        winner: false,
+        open: false,
+        chosen: false
+    }]
+
+    gameState.winningDoor = Math.floor(Math.random() * doors.length)
+    doors[gameState.winningDoor].winner = true
+    gameState.phase = 1;
+}
+
+document.onload = start()
